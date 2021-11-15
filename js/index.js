1f71ab08c99bf854753b2d2d1baa9bec2e3f9764
patricia-benites <patricia.benites.cava@gmail.com>
2021-11-15 11:00:08 -0300

    add iteration 3

--- js/index.js
+++ js/index.js
@@ -20,3 +20,36 @@ if (driverLength > navigatorLength){
   console.log(`Wow, you both have equally long names, ${driverLength} characters!`)
 }
 // Iteration 3: Loops
+// 3.1
+let driversName = '';
+
+for (let i = 0; i < driverLength; i++){
+
+  if (i === driverLength - 1){
+    driversName+= `${hacker1[i].toUpperCase()}`;
+  } else {
+    driversName+= `${hacker1[i].toUpperCase()} `;
+  }
+
+}
+console.log(driversName);
+
+// 3.2
+
+let reverseName = '';
+
+for (let i = driverLength-1; i>=0; i--){
+  reverseName+= hacker1[i]
+}
+
+console.log(reverseName);
+
+// 3.3
+
+if (hacker1.localeCompare(hacker2) === 0) {
+    console.log("What?! You both have the same name?");
+    } else if (hacker1.localeCompare(hacker2) === -1) {
+      console.log("The driver's name goes first.")
+    } else if (hacker1.localeCompare(hacker2) === 1) {
+      console.log("Yo, the navigator goes first definitely.")
+    }
